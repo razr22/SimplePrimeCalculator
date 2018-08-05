228641288fbd95b5ed23b54ae54389a954051230
Zain Quraishi <zain.quraishi@live.ca>
2018-08-05 16:32:07 -0400

    initial file push

---
+++ getPrimes.py
@@ -0,0 +1,63 @@
+#author: Zain Quraishi
+#date: 08/05/2018
+#description: Simple python script that allows the user to compute specific prime values or return a list of prime values given a search space.
+
+import os
+
+#function generates list of all primes within given range and returns list
+def getPrimes(ceiling):
+	#generate list of candidate numbers
+	candidate_list = []
+	for i in range (2, ceiling + 1):
+		candidate_list.append(i)
+
+#	print ("Search Space ... " + str(candidate_list))
+
+	#parse candidates for primes
+	result = []
+	j = 0
+	while (j < len(candidate_list)):
+		m = j
+		while m < len(candidate_list):
+			temp = candidate_list[m]
+			index = candidate_list[j]
+			if temp % index == 0 and temp != index:
+				candidate_list.remove(temp)			
+			m += 1
+		j += 1
+	return candidate_list
+
+
+if __name__ == '__main__':
+	clear = lambda: os.system('clear')
+	
+	print ("--- Selection Menu ---")
+	
+	#user can choose which operation to perform
+	option = input ("1. Compute primes in specified search space\n2. Check if number is prime\nEnter Selection (1 or 2): ")	
+
+	if (int(option) == 1):
+		clear()
+		print ("--- Check for Primes in Search Space (Max: 500) ---")
+		limit = input ("Enter prime search space quantity: ")
+		
+		#check for legal input
+		if int(limit) > 0 and int(limit) < 501:
+			resulting_primes = getPrimes(int(limit))
+			print ("Resulting Primes ... \n" + str(resulting_primes))
+		else:
+			print ("Error! Invalid Input...")
+	else:
+		clear()
+		print ("--- Check if a Number is Prime (Max: 1000) ---")
+		check_number = input ("Enter Number : ")
+		
+		#check for legal input
+		if int(check_number) < 1001 and int(check_number) > -1:
+			verdict = getPrimes(int(check_number))
+			if int(check_number) in verdict:
+				print (str(check_number) + " is PRIME")
+			else:
+				print (str(check_number) + " is NOT PRIME")
+		else: 
+			print("Error! Invalid Input...")
